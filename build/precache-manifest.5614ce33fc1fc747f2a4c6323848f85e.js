58d780a59d7429ab0056611d58436347df786e96
SykaaInc <97787838+Sykaa-Platforms@users.noreply.github.com>
2022-10-16 13:44:03 +0530

    Production-build-fix

---
+++ build/precache-manifest.5614ce33fc1fc747f2a4c6323848f85e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844145b37d651d6ac00efd85121ce5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f72322e6948441c112",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a00bc4cedb4abc21389c",
+    "url": "/static/js/2.b74a6929.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/static/js/2.b74a6929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f72322e6948441c112",
+    "url": "/static/js/main.69ae9a6a.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/static/media/victory.6db44b04.png"
+  }
+]);
